8b93b382f0328fc25f81bf5df411cf00aa9fbd37
JoelSuh07 <joel.suh07@gmail.com>
2023-08-20 00:30:06 -0700

    commit

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Enter a bread number.");
+        
+        new MyFrame();
+
+        //System.out.println("Enter a bread number.");
+
     }
 }
