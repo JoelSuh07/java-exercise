90fcfd79bc2c3add5c30e9405873cab51824062a
JoelSuh07 <joel.suh07@gmail.com>
2023-08-24 00:17:00 -0700

    commit

--- MyFrame.java
+++ MyFrame.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -13,5 +16,11 @@ public class MyFrame extends JFrame implements ActionListener{
         this.setSize(500, 500);
         this.setVisible(true);
         this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
