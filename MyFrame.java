76762857b7103d33b21d08a368d4aeba92e718f9
JoelSuh07 <joel.suh07@gmail.com>
2023-08-25 16:51:53 -0700

    all button added

--- MyFrame.java
+++ MyFrame.java
@@ -11,15 +11,36 @@ public class MyFrame extends JFrame implements ActionListener{
     MyFrame(){
 
         button = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        button6 = new JButton();
+        button7 = new JButton();
+        button8 = new JButton();
         button.setBounds(200, 100, 100, 50);
         button.addActionListener(this);
         button.setText("Button");
+        button2.setText("Button2");
+        button3.setText("Button3");
+        button4.setText("Button4");
+        button5.setText("Button5");
+        button6.setText("Button6");
+        button7.setText("Button7");
+        button8.setText("Button8");
         button.setFocusable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
         this.add(button);
+        this.add(button2);
+        this.add(button3);
+        this.add(button4);
+        this.add(button5);
+        this.add(button6);
+        this.add(button7);
+        this.add(button8);
     }
 
     @Override
